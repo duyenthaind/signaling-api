184f01246b46a7f136fc2bdb63f47910e06e459e
duyenthaind <duyenthai123456@gmail.com>
2022-04-09 12:35:28 +0700

    init project

---
+++ src/main/java/org/example/signalingapi/SignalingApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.signalingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/signalingapi/SignalingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.signalingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
